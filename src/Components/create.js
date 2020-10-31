3c56de5b2bbd6f9cbd31bb4b337976cb59d16ec4
Daniel <G00359991@gmit.ie>
2020-10-31 08:09:39 +0000

    lab completed

--- src/Components/create.js
+++ src/Components/create.js
@@ -2,11 +2,77 @@ import React from 'react'; // pulls in code from react to be used in create.js a
 import '../App.css';
 
 export class Create extends React.Component { // extends from and uses the code from react
+    constructor() {
+        super();
+
+        this.onSubmit = this.onSubmit.bind(this); // binds the code for the submit button to allow it to submit the contents entered
+        this.onChangeTitle = this.onChangeTitle.bind(this); // binds the code for a movie title to be inputted and then submitted from the create.js page in the react app
+        this.onChangeYear = this.onChangeYear.bind(this); // binds the code for a movie year to be inputted and then submitted from the create.js page in the react app
+        this.onChangePoster = this.onChangePoster.bind(this); // binds the code for a movie poster to be inputted and then submitted from the create.js page in the react app
+
+        this.state = { /*calls the title, year and poster values to the screen */
+            Title: '',
+            Year: '',
+            Poster: ''
+        }
+    }
+
+    onChangeTitle(e) { /* calls the title method for the create.js page to allow the information inputted to be outputted*/
+        this.setState({
+            Title: e.target.value
+        });
+    }
+
+    onChangeYear(e) { /* calls the year method for the create.js page to allow the information inputted to be outputted*/
+        this.setState({
+            Year: e.target.value
+        });
+    }
+
+    onChangePoster(e) { /* calls the poster method for the create.js page to allow the information inputted to be outputted*/
+        this.setState({
+            Poster: e.target.value
+        });
+    }
+    onSubmit(e) { /*calls the submit button's method on the create.js page to submit the information inputted */
+        e.preventDefault();
+        alert("Movie: " + this.state.Title + " "
+            + this.state.Year + " " +
+            this.state.Poster);
+    }
 
     render() {
-        return ( // outputs the heading text for the create.js to the screen
+        return ( /* outputs the forms into the create.js page on screen to have a new movie title, year, and poster outputted in via a button. */
             <div className="App">
-                <h1>My create is another component</h1>
+                <form onSubmit={this.onSubmit}>
+                    <div className="form-group">
+                        <label>Add Movie Title: </label>
+                        <input type='text'
+                            className='form-control'
+                            value={this.state.Title}
+                            onChange={this.onChangeTitle}></input>
+                    </div>
+                    <div className="form-group">
+                        <label>Add Movie Year: </label>
+                        <input type='text'
+                            className='form-control'
+                            value={this.state.Year}
+                            onChange={this.onChangeYear}></input>
+                    </div>
+                    <div className="form-group">
+                        <label>Movies Poster: </label>
+                        <textarea type='text'
+                            className='form-control'
+                            value={this.state.Poster}
+                            onChange={this.onChangePoster}>
+                        </textarea>
+                    </div>
+                    <div className='form-group'>
+                        <input type='submit'
+                            value='Add Movie'
+                            className='btn btn-primary'></input>
+                    </div>
+                </form>
             </div>
         );
     }
--- src/Components/read.js
+++ src/Components/read.js
@@ -1,36 +1,28 @@
 import React from 'react'; // pulls in code from react to be used in read.js and allow for the html to be outputted into the react app
 import '../App.css';
 import { Movies } from './movies'; // pulls in code from the movies.js component to be used in read.js, which will then be brought in and used for App.js
+import axios from 'axios'; // pulls in code from axios to be used in read.js to allow for json to be outputted correctly into the react app
 
 export class Read extends React.Component { // extends from and uses the code from react
 
     state = {
-        movies: [ /*code below outputs the title, year of release, and image/poster of each movie to the screen*/
-            {
-            "Title": "Avengers: Infinity War",
-            "Year": "2018",
-            "imdbID": "tt4154756",
-            "Type": "movie",
-            "Poster": "https://m.media-amazon.com/images/M/MV5BMjMxNjY2MDU1OV5BMl5BanBnXkFtZTgwNzY1MTUwNTM@._V1_SX300.jpg"
-            },
-            {
-            "Title": "Captain America: Civil War",
-            "Year": "2016",
-            "imdbID": "tt3498820",
-            "Type": "movie",
-            "Poster": "https://m.media-amazon.com/images/M/MV5BMjQ0MTgyNjAxMV5BMl5BanBnXkFtZTgwNjUzMDkyODE@._V1_SX300.jpg"
-            },
-            {
-            "Title": "Charlie Wilson's War",
-            "Year": "2007",
-            "imdbID": "tt0472062",
-            "Type": "movie",
-            "Poster": "https://m.media-amazon.com/images/M/MV5BMTgwMDgwMDc4MF5BMl5BanBnXkFtZTYwOTU3MDM4._V1_SX300.jpg"
-            }
-            ]            
+        movies: []
     };
+
+    componentDidMount() { /* fetches the necessary jsonblob information to be outputted in read.js in order to acquire the necessary data for the movies to be outputted to the screen. An error message to pop up in case something goes wrong. */
+        axios.get('https://jsonblob.com/api/jsonblob/520c3b5e-0312-11eb-a6af-cbf00d776032')
+            .then(
+                (response) => {
+                    this.setState({ movies: response.data.Search })
+                }
+            )
+            .catch((error) => {
+                console.log(error)
+            });
+    }
+
     render() {
-        return ( /*outputs the heading text for the read.js to the screen, and also runs code that allows for the code from movies.js to run properly and output to the screen.*/ 
+        return ( /*outputs the heading text for the read.js to the screen, and also runs code that allows for the code from movies.js to run properly and output to the screen.*/
             <div className="App">
                 <h1>My read is another component</h1>
                 <Movies movies={this.state.movies}></Movies>
